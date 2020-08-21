481920f78f664798f1ea07645040196b94b22e4d
almog gueta <almog.gueta@live.biu.ac.il>
2020-08-21 12:39:28 +0300

    logistic regresison - hyper parameters

--- logisticRegressionFB.py
+++ logisticRegressionFB.py
@@ -8,10 +8,10 @@ from sklearn import metrics
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 
-
 import numpy as np
 
-def draw_class(y_test,y_pred):
+
+def draw_class(y_test, y_pred):
     cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
     cnf_matrix
 
@@ -30,19 +30,20 @@ def draw_class(y_test,y_pred):
 
     plt.show()
 
+
 def check_hyper_parameters(data_features, data_tags):
-    penalty = [ 'l2', 'elasticnet', 'none']
-    c_value = [2, 1, 0.5, 0.1 , 0.001, 0.0001, 0.02 , 0.005, 0.0005]
-    solver = ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
-    max_iter = [10000, 20000, 50000, 100000]
+    penalty = ['l2']
+    c_value = [0.0005, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
+    solver = ['newton-cg', 'lbfgs', 'sag', 'saga']
+    max_iter = [100, 1000, 100000, 1000000]
 
     parms = []
     train = []
     valid = []
     all_combinations = [[i, j, k, z] for i in penalty
-                 for j in c_value
-                 for k in solver
-                 for z in max_iter]
+                        for j in c_value
+                        for k in solver
+                        for z in max_iter]
 
     for penalty, c_value, solver, max_iter in all_combinations:
         print(f"penately: {penalty}, C: {c_value}, solver: {solver}, max_iter: {max_iter}")
@@ -58,8 +59,7 @@ def check_hyper_parameters(data_features, data_tags):
           f" train accuracy={train[best_index]}, valid accuracy={valid[best_index]}")
 
 
-def cross_validation(data_features, data_tags,penalty,c_value, solver, max_iter):
-
+def cross_validation(data_features, data_tags, penalty, c_value, solver, max_iter):
     train_acc_cross, valid_acc_cross = [], []
 
     from sklearn.model_selection import StratifiedKFold
@@ -76,7 +76,8 @@ def cross_validation(data_features, data_tags,penalty,c_value, solver, max_iter)
         validation_data = data_features[valid_index]
         validation_tags = data_tags[valid_index]
 
-        logreg = LogisticRegression(penalty=penalty,C=c_value,solver=solver, max_iter=max_iter)
+        logreg = LogisticRegression(penalty=penalty, C=c_value, solver=solver, max_iter=max_iter,
+                                    multi_class='multinomial')
 
         # Train Logistic regression Tree Classifer
         logreg = logreg.fit(train_data, train_tags)
@@ -88,20 +89,18 @@ def cross_validation(data_features, data_tags,penalty,c_value, solver, max_iter)
         train_acc_cross.append(metrics.accuracy_score(train_tags, train_predictions))
         valid_acc_cross.append(metrics.accuracy_score(validation_tags, validation_predictions))
 
-
     print(f"max train accuracy: {round((max(train_acc_cross)).item(), 3)},"
           f" max validation accuracy: {round((max(valid_acc_cross)).item(), 3)}")
     from statistics import mean
-    train_mean = round((mean(train_acc_cross)).item(),3)
-    valid_mean = round((mean(valid_acc_cross)).item(),3)
+    train_mean = round((mean(train_acc_cross)).item(), 3)
+    valid_mean = round((mean(valid_acc_cross)).item(), 3)
     print(f"average train accuracy: {train_mean},"
-           f" average validation accuracy: {valid_mean}")
+          f" average validation accuracy: {valid_mean}")
     return train_mean, valid_mean
 
 
 def main():
-
-    train_file = "data/processedDataAll.csv"
+    train_file = "data/train.csv"
     # load dataset
     col_names = ['Page total likes', 'Type', 'Category', 'Post Month',
                  'Post Weekday', 'Post Hour', 'Paid', 'Lifetime Post Total Reach',
@@ -121,16 +120,16 @@ def main():
     X = pima[feature_cols]  # Features
     y = pima.like  # Target variable
 
-    #check
-    check_hyper_parameters(X, y)
+    # check
+    #   check_hyper_parameters(X, y)
+    cross_validation(X, y, 'none', 0.1, 'newton-cg', 100)
 
-  #  logreg = LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, fit_intercept=True,
-  #                              intercept_scaling=1, class_weight=None, random_state=None, solver='liblinear',
-  #                              max_iter=10000, multi_class='ovr', verbose=0)
+    #  logreg = LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, fit_intercept=True,
+    #                              intercept_scaling=1, class_weight=None, random_state=None, solver='liblinear',
+    #                              max_iter=10000, multi_class='ovr', verbose=0)
     # Split dataset into training set and test set
 
     print("hello")
-
 
 
 if __name__ == '__main__':
